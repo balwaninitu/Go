be8f871345b17dd7066d4c0bd25226cb98c45d83
Nitu Balwani <balwaninitu@gmail.com>
2021-02-15 23:46:28 +0800

    Go Advanced

---
+++ src/Assignments/goschool-assignment-2/Dental-Appointment-Syastem/doctor.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+//sequential search algorithm is used to search available docotor selected by user
+//user will get prompted based on availability of doctor
+func searchDoctorByName(doctorList *[]doctorDetails, doctorName string) bool {
+	var available bool = false
+	var tempDoctorName = strings.ToUpper(strings.TrimSpace(doctorName))
+	fmt.Println()
+	for index, doctorValue := range *doctorList {
+		if strings.ToUpper(strings.TrimSpace(doctorValue.doctorName)) == tempDoctorName && doctorValue.available {
+			fmt.Printf("%d) %s %v\n", index+1, doctorValue.doctorName, doctorValue.DayTime.Format(time.ANSIC))
+			available = true
+		}
+	}
+	if !available {
+		fmt.Printf("Docotr %s not found\n", tempDoctorName)
+		return available
+	}
+
+	return available
+}
+
+//function to display available doctors time slot to book for appointment.
+//Doctor deatils is avaialble in slice, loop will range over to find all available doctors to display
+func displayAllDoctorAvailableTime(doctorList []doctorDetails) {
+	fmt.Println()
+	fmt.Println("***List of available docotrs***")
+	fmt.Println(strings.Repeat("=", 40))
+	for index, doctorValue := range doctorList {
+		if doctorValue.available {
+			fmt.Printf("%d) %s %s %d\n", index+1, doctorValue.doctorName, doctorValue.DayTime.Format(time.ANSIC), doctorValue.appointmentID)
+		}
+	}
+}
+
+//ReadCsv can be exported
+func ReadCsv(filename string) ([][]string, error) {
+
+	f, err := os.Open(filename)
+	if err != nil {
+		return [][]string{}, err
+	}
+	defer f.Close()
+
+	lines, err := csv.NewReader(f).ReadAll()
+	if err != nil {
+		return [][]string{}, err
+	}
+
+	return lines, nil
+}
+
+// ReadDoctorList can be exported
+func readDoctorList(doctorList *[]doctorDetails) []doctorDetails {
+	var lines, err = ReadCsv("C:/Projects/Go/src/Assignments/goschool-assignment-2/Dental-Appointment-Syastem/doctor.csv")
+	if err != nil {
+		panic(err)
+	}
+	for _, line := range lines {
+		drID, err := strconv.Atoi(line[0])
+		check(err)
+		doctorName := line[1]
+		DayTime, err := time.Parse(time.RFC822, line[2])
+		check(err)
+		available, err := strconv.ParseBool(line[3])
+		check(err)
+		data := doctorDetails{
+			drID:       drID,
+			doctorName: doctorName,
+			DayTime:    DayTime,
+			available:  available,
+		}
+
+		*doctorList = append(*doctorList, data)
+	}
+	return *doctorList
+}
---
+++ src/Assignments/goschool-assignment-2/Dental-Appointment-Syastem/input.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// Function #1 :- Display user choice list and accept user input
+func displayList() int {
+	fmt.Println()
+	fmt.Println(strings.TrimSpace(title))
+	fmt.Println(strings.Repeat("=", 40))
+	fmt.Print(strings.TrimSpace(listDisplay))
+	var userListInput int
+	_, err := fmt.Scanln(&userListInput)
+	if err != nil {
+		fmt.Println(errors.New("Error:No input"))
+	}
+	return userListInput
+}
+
+// Function#2 :- Call appropriate sub-function based on user Input
+func userChoiceAction(userListInput int, doctorList *[]doctorDetails, appointmentList *ClinicAppointmentList) {
+	ch := make(chan string)
+	go readAdminPassword(ch) //go routine created to receive and transfer password securely through channel for admin only features
+	switch userListInput {
+	case 1:
+		*doctorList = makeAppointment(&(*doctorList), appointmentList)
+	case 2:
+		displayAllDoctorAvailableTime(*doctorList)
+	case 3:
+		var doctorName string
+
+		fmt.Print("Enter Doctor Name: ")
+		_, err := fmt.Scanln(&doctorName)
+		if err != nil {
+			fmt.Println(errors.New("Error:No input"))
+		}
+
+		_ = searchDoctorByName(&(*doctorList), doctorName)
+	case 4:
+		displayAllBookedAppointments(appointmentList)
+		//admin features only accessible after entering password which is concurrentltly running through goroutines channel
+		//password will be send by channel
+	case 5:
+		readPassword := <-ch
+		enteredPassword := enterAdminPassword()
+		if enteredPassword == readPassword {
+			fmt.Println("Success")
+			deleteAppointment(appointmentList)
+		} else {
+			fmt.Println("Incorrect password")
+		}
+
+	case 6:
+		readPassword := <-ch
+		enteredPassword := enterAdminPassword()
+		if enteredPassword == readPassword {
+			fmt.Println("Success")
+			displayAllDoctorAvailableTime(*doctorList)
+			creatDoctorSchedule(&(*doctorList))
+		} else {
+			fmt.Println("Incorrect password")
+		}
+
+	case 7:
+		fmt.Println("Bye! Bye!")
+		os.Exit(0)
+	}
+}
